6e69f451234a3a37f5a09445f6f5e3e884dc92dd
Darby Biddy <darbybiddy3@gmail.com>
2023-01-12 17:05:42 -0600

    pushing navigationg

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,1 +1,33 @@
 import React from "react";
+import Footer from "../Footer";
+
+function About() {
+  return (
+    <section>
+      <div className="about">
+        <h2 className="subtitle">Professional Portfolio</h2>
+        <img src="" className="" alt=""></img>
+      </div>
+      <div id="personal" className="aboutme">
+        <div className="about-title">
+          <h2>About Me</h2>
+        </div>
+        <div>
+          <img src="" alt="profile picture"></img>
+          <p>
+            Hello, I am Darby Biddy and im a beginner Software Developer. I am
+            from Iowa Park Texas. I went to school at Hardin-Simmons University
+            where I got my degree in Graphic Design. I plan on doing some
+            personal projects before starting in this career field. I took a
+            Fullstack software development class for 6 months where I learned
+            all types of programs. in this class we focused on HTML, CSS,
+            JavaScript, React, Node.js, SQL, NoSQL, Express.js, and API's.
+          </p>
+        </div>
+      </div>
+      <footer></footer>
+    </section>
+  );
+}
+
+export default About;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,1 +1,31 @@
 import React from "react";
+
+function Nav(){
+    return(
+       <header>
+        <h2 className="header-title"> 
+            <a data-testid="link" href="/">
+                <span role= "img" aria-label="camera"> 📸 Darbys React Portfolio</span>
+            </a>
+        </h2>
+        <nav>
+            <ul className="flex-row">
+                <li className="mx-3">
+                    <a href="#about">About Me</a>
+                </li>
+                <li className="mx-3">
+                    <a href="#portfolio">Portfolio</a>
+                </li>
+                <li className="mx-3">
+                    <a href="#contact">Contact</a>
+                </li>
+                <li className="mx-3">
+                    <a href="#resume">Resume</a>
+                </li>
+            </ul>
+        </nav>
+       </header>
+    )
+}
+
+export default Nav
